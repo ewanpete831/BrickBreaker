e6f13109d0b53db69ccaf545bc7e1101508a6ab8
ewanpete831 <ewanpete831@outlook.com>
2022-05-18 08:59:52 -0400

    arstdyfuiog

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace BrickBreaker.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -164,34 +164,36 @@ namespace BrickBreaker.Properties {
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
         ///&lt;Bricks&gt;
         ///	&lt;Brick&gt;
-        ///		&lt;x&gt;375&lt;/x&gt;
-        ///		&lt;y&gt;89&lt;/y&gt;
+        ///		&lt;x&gt;544&lt;/x&gt;
+        ///		&lt;y&gt;57&lt;/y&gt;
         ///		&lt;hp&gt;1&lt;/hp&gt;
-        ///		&lt;colour&gt;Color [DarkRed]&lt;/colour&gt;
+        ///		&lt;colour&gt;DarkRed&lt;/colour&gt;
         ///	&lt;/Brick&gt;
         ///	&lt;Brick&gt;
-        ///		&lt;x&gt;390&lt;/x&gt;
-        ///		&lt;y&gt;89&lt;/y&gt;
+        ///		&lt;x&gt;762&lt;/x&gt;
+        ///		&lt;y&gt;352&lt;/y&gt;
         ///		&lt;hp&gt;1&lt;/hp&gt;
-        ///		&lt;colour&gt;Color [DarkRed]&lt;/colour&gt;
+        ///		&lt;colour&gt;DarkRed&lt;/colour&gt;
         ///	&lt;/Brick&gt;
         ///	&lt;Brick&gt;
-        ///		&lt;x&gt;375&lt;/x&gt;
-        ///		&lt;y&gt;73&lt;/y&gt;
-        ///		&lt;hp&gt;1&lt;/hp&gt;
-        ///		&lt;colour&gt;Color [DarkRed]&lt;/colour&gt;
+        ///		&lt;x&gt;125&lt;/x&gt;
+        ///		&lt;y&gt;143&lt;/y&gt;
+        ///		&lt;hp&gt;2&lt;/hp&gt;
+        ///		&lt;colour&gt;LightGray&lt;/colour&gt;
         ///	&lt;/Brick&gt;
         ///	&lt;Brick&gt;
-        ///		&lt;x&gt;390&lt;/x&gt;
-        ///		&lt;y&gt;73&lt;/y&gt;
-        ///		&lt;hp&gt;1&lt;/hp&gt;
-        ///		&lt;colour&gt;Color [DarkRed]&lt;/colour&gt;
+        ///		&lt;x&gt;125&lt;/x&gt;
+        ///		&lt;y&gt;119&lt;/y&gt;
+        ///		&lt;hp&gt;2&lt;/hp&gt;
+        ///		&lt;colour&gt;LightGray&lt;/colour&gt;
         ///	&lt;/Brick&gt;
         ///	&lt;Brick&gt;
-        ///		&lt;x&gt;390&lt;/x&gt;
-        ///		&lt;y&gt;48&lt;/y&gt;
-        ///		&lt;hp&gt;1&lt;/hp&gt;
-        ///		&lt;colour&gt;Color [Da [rest of string was truncated]&quot;;.
+        ///		&lt;x&gt;125&lt;/x&gt;
+        ///		&lt;y&gt;105&lt;/y&gt;
+        ///		&lt;hp&gt;2&lt;/hp&gt;
+        ///		&lt;colour&gt;LightGray&lt;/colour&gt;
+        ///	&lt;/Brick&gt;
+        ///	&lt; [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Level2 {
             get {
